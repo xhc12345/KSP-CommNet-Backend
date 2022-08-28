01ca7b84dd754344ed6d0aed205cfe49add8137c
xhc12345 <kevin20020613@gmail.com>
2022-08-28 03:45:46 -0500

    working database and csv logics

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+import os
+import glob
+from pathlib import Path
+import yaml
+import csv
+from flask import Flask, render_template, request, redirect, url_for, jsonify, send_file
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
+
+def getProjectRoot() -> Path:
+    return str(Path(__file__).parent)
+
+def loadConfig():
+    with open("config.yaml", "r") as stream:
+        try:
+            config = yaml.safe_load(stream)
+            return config
+        except yaml.YAMLError as exc:
+            print(exc)
+
+print("=== loading yaml config")
+config = loadConfig()
+print("=== done with yaml")
 
 app = Flask(__name__)
 
@@ -7,12 +28,35 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class KSP_Planet(db.Model):
+class KSP_CelestialBody(db.Model):
+    __tablename__ = 'KSP_CelestialBody'
     id = db.Column(db.Integer, primary_key=True)
-    planetName = db.Column(db.String(100))
+    name = db.Column(db.String(63), index=True, unique=True)
+    mass = db.Column(db.Integer, index=True)
+    radius = db.Column(db.Integer, index=True)
+
+def loadCelestialBodyData():
+    fileName = config['data']['celestialBodies']['fileName']
+    fileExtension = config['data']['celestialBodies']['fileExtension']
+    fullFile = fileName+'.'+fileExtension
+    dataFile = getProjectRoot()+'\\'+config['data']['path']+'\\'+fullFile
+    print('==== loading up file at: '+dataFile)
+    fields = []
+    with open(dataFile, 'r') as csvFile:
+        csvReader = csv.reader(csvFile)
+        fields = next(csvReader)
+        for row in csvReader:
+            db.session.add(KSP_CelestialBody(name=row[0], mass=row[1], radius=row[2]))
+    db.session.commit()
+    print('==== finished feeding db with file data')
+
+@event.listens_for(KSP_CelestialBody.__table__, 'after_create')
+def db_init_planets(*args, **kwargs):
+    print("### Initializing database table ###")
+    loadCelestialBodyData()
+    print("### Finished populating database ###")
 
 db.create_all()
-
 
 
 @app.get("/", strict_slashes=False)
@@ -21,8 +65,25 @@ def home():
 
 @app.route("/planets", strict_slashes=False)
 def allPlanets():
-    planetList = db.session.query(KSP_Planet).all()
+    planetsQuery = db.session.query(KSP_CelestialBody).all()
+    planetList = []
+    for planet in planetsQuery:
+        planetList.append({ 'name':     planet.name,
+                            'mass':     planet.mass,
+                            'radius':   planet.radius})
     return planetList
+
+@app.route('/image/', strict_slashes=False)
+def get_image_none():
+    return "no image for you today lol"
+
+@app.route('/image/<imageName>', strict_slashes=False)
+def get_image(imageName):
+    imgFolder = getProjectRoot() + "\\assets\img\\"
+    filePath = imgFolder+imageName
+    print('requesting '+filePath)
+    for infile in glob.glob( os.path.join(imgFolder, imageName+'.*') ):
+        return send_file(infile, mimetype='image/gif')
 
 @app.errorhandler(404)
 def invalidRequest(e):
